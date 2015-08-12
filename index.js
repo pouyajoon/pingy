3b8dfc43f3e9e1bf5ab4cf73abbd573a95a17e27
Pouya <pouyajoon@gmail.com>
2015-08-12 23:31:11 +0200

    update sites

---
+++ index.js
@@ -0,0 +1,52 @@
+(function() {
+  'use strict';
+
+
+
+  function getSiteContent(src) {
+    var http = require('http');
+    // console.info('get', src);
+    return http.get({
+      host: src,
+      path: '/'
+    }, function(response) {
+      // Continuously update stream with data
+      var body = '';
+      response.on('data', function(d) {
+        body += d;
+      });
+      response.on('end', function() {
+        console.info('get ', src, 'ends', body.length);
+      });
+    });
+  }
+
+  function getSite(src) {
+    setTimeout(function() {
+      getSiteContent(src);
+    }, 0);
+  }
+
+  var later = require('later'),
+    cron;
+  cron = later.parse.cron('0,10,20,30,40,50 7-23 * * * *');
+
+  // function to execute
+  function logTime() {
+    console.log(new Date());
+  }
+
+  function getSites() {
+    logTime();
+    var i, sites;
+    sites = ['www.kaarchi.fr', 'demo.surfy.pro', 'mdm.surfy.pro', 'www.surfy.pro'];
+    for (i = 0; i < sites.length; i += 1) {
+      getSite(sites[i]);
+    }
+  }
+
+  // execute logTime for each successive occurrence of the text schedule
+  later.setInterval(getSites, cron);
+
+
+}());
